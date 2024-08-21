f09641203385104191cf52848e285c0c2db135c8
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-22 00:55:40 +0530

    called the functions in pages

---
+++ src/app/pages/LoginPage/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { LoginForm } from './login/page';
+
+const Page = () => {
+  return (
+    <div>
+    <LoginForm/>
+    </div>
+  );
+};
+export default Page;
---
+++ src/app/pages/LoginPage/signup/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { SignupForm } from "@/app/pages/LoginPage/signup/signup";
+
+const Page = () => {
+  return (
+    <div>
+    <SignupForm/>
+    </div>
+  );
+};
+export default Page;
