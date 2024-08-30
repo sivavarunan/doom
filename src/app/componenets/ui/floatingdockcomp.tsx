daf341c9b707d8e49d17dfb814aa6f9ecbf31900
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-08-30 06:16:20 +0530

    implemented the floating doc as a functional component

---
+++ src/app/componenets/ui/floatingdockcomp.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { FloatingDock } from "./floating-doc";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+  IconFile,
+} from "@tabler/icons-react";
+import Image from "next/image";
+
+export function FloatingDockComp({ className = '' }: { className?: string }) {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "DOOM",
+      icon: (
+        <Image
+          src="/doom1.png"
+          width={500}
+          height={200}
+          alt="Aceternity Logo"
+        />
+      ),
+      href: "#",
+    },
+    {
+      title: "File",
+      icon: (
+        <IconFile className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+
+  return (
+    <div className={`flex items-center justify-center ${className}`}>
+      <FloatingDock
+        items={links}
+      />
+    </div>
+  );
+}
